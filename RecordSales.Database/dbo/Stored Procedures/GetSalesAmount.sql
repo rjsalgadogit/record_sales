0349fafc25978d1ca8abc2dd2f13a83ee3b1ff17
Ros Jason Salgado <ros-jason.salgado@besix.com>
2023-02-04 00:13:27 +0800

    Update DB (2)

---
+++ RecordSales.Database/dbo/Stored Procedures/GetSalesAmount.sql
@@ -0,0 +1,15 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE GetSalesAmount
+	@Code nvarchar(128)
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    SELECT SUM(Amount) as TotalSales FROM CashFLow WHERE Code = @Code
+END
